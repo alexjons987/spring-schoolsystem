fa7068b886fdef44594f935e500e1eb4cf4aca7c
alexjons987 <alexander.jonsson987@outlook.com>
2025-11-10 11:52:14 +0100

    Implement Student service

--- schoolsystem/src/main/java/se/javapp/schoolsystem/service/StudentService.java
+++ schoolsystem/src/main/java/se/javapp/schoolsystem/service/StudentService.java
@@ -3,9 +3,36 @@ package se.javapp.schoolsystem.service;
 import org.springframework.stereotype.Service;
 import se.javapp.schoolsystem.model.Student;
 import se.javapp.schoolsystem.model.dto.StudentDTO;
+import se.javapp.schoolsystem.repository.StudentRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<StudentDTO> getAllStudents() {
+        List<StudentDTO> studentDTOs = new ArrayList<>();
+
+        for (Student student : studentRepository.getAll()) {
+            studentDTOs.add(this.toDTO(student));
+        }
+
+        return studentDTOs;
+    }
+
+    public Optional<StudentDTO> createStudent(StudentDTO studentDTO) {
+        Student student = this.toEntity(studentDTO);
+        studentRepository.save(student);
+
+        return Optional.of(this.toDTO(student));
+    }
 
     private StudentDTO toDTO(Student student) {
         if (student == null) return null;
